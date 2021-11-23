b13f53afab86c7b19f8b96e4e73406ef850d0c3c
Nikita <zhuoz@protonmail.com>
2021-11-22 19:05:43 -0500

    more ADPCMEncoderConfig tests

--- test/com/jiggawatt/jt/tools/adpcm/ADPCMEncoderConfigTest.java
+++ test/com/jiggawatt/jt/tools/adpcm/ADPCMEncoderConfigTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.jiggawatt.jt.tools.adpcm.ADPCMEncoderConfig.AUTO_BLOCK_SIZE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ADPCMEncoderConfigTest {
 
@@ -85,5 +88,71 @@ public class ADPCMEncoderConfigTest {
             IllegalArgumentException.class,
             () -> ADPCMEncoder.configure().setBlockSize(65536)
         );
+    }
+
+    @Test
+    public void appliesNoiseShapingSetting() {
+        assertFalse(
+            ADPCMEncoder.configure()
+                .setNoiseShaping(false)
+                .end()
+            .getNoiseShaping()
+        );
+    }
+
+    @Test
+    public void expectedDefaults() {
+        ADPCMEncoderConfig def = ADPCMEncoder.configure().end();
+
+        assertEquals(2,     def.getChannels());
+        assertEquals(44100, def.getSampleRate());
+        assertTrue  (def.getNoiseShaping());
+    }
+
+    @Test
+    public void computesBlockSizeByDefault() {
+        doTestAutoBlockSize(ADPCMEncoder.configure());
+    }
+
+    @Test
+    public void computesBlockSizeForAutoSetting() {
+        doTestAutoBlockSize(ADPCMEncoder.configure().setBlockSize(AUTO_BLOCK_SIZE));
+    }
+
+    private void doTestAutoBlockSize(ADPCMEncoderConfig.Builder builder) {
+        assertEquals(
+            256,
+            builder
+                .setSampleRate(8000)
+                .setChannels(1)
+                .end()
+            .getBlockSize()
+        );
+
+        assertEquals(
+            2048,
+            builder
+                .setSampleRate(44100)
+                .setChannels(2)
+                .end()
+            .getBlockSize()
+        );
+    }
+
+    @Test
+    public void copyFactoryReproducesAllSettings() {
+        ADPCMEncoderConfig src = ADPCMEncoder.configure()
+            .setChannels(1)
+            .setSampleRate(8000)
+            .setNoiseShaping(false)
+            .setBlockSize(256)
+            .end();
+
+        ADPCMEncoderConfig cpy = ADPCMEncoder.configure(src).end();
+
+        assertEquals(src.getChannels(),     cpy.getChannels());
+        assertEquals(src.getSampleRate(),   cpy.getSampleRate());
+        assertEquals(src.getNoiseShaping(), cpy.getNoiseShaping());
+        assertEquals(src.getBlockSize(),    cpy.getBlockSize());
     }
 }
